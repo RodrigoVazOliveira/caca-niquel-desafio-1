10258191a37e002371e5ab3755ba25f8b8d67597
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-12 13:59:31 -0300

    implementando class de sistema resonsavel por gerenciar o sistema total

--- src/br/dev/rvz/Jogo.java
+++ src/br/dev/rvz/Jogo.java
@@ -1,5 +1,6 @@
 package br.dev.rvz;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -10,14 +11,17 @@ import java.util.Random;
 public class Jogo {
 
     private int numeroAleatorio;
+    private List<ValoresSorteio> acertos;
     private int ponto;
 
     public Jogo() {
         this.ponto = 0;
+        acertos = new ArrayList<>();
     }
 
     public int sortear() {
         numeroAleatorio = new Random().nextInt(ValoresSorteio.values().length);
+        acertos.add(ValoresSorteio.values()[numeroAleatorio]);
         return ValoresSorteio.values()[numeroAleatorio].ponto;
     }
 
--- src/br/dev/rvz/Sistema.java
+++ src/br/dev/rvz/Sistema.java
@@ -1,8 +1,14 @@
 package br.dev.rvz;
 
+/**
+ *  rsponsavel pelo gerenciamento de pontos e interafção com o usuário
+ * */
 public class Sistema {
 
     private boolean run;
     private int opcao;
 
+    public void menuPrincipal() {
+
+    }
 }
